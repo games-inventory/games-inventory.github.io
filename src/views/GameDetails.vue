ae0b9fddf589e287455f96e18306c3bec93a77c6
Theodore Leebrant <theodoreleebrant@gmail.com>
2022-01-08 00:14:07 +0800

    Add axios call to popualte game details

--- src/views/GameDetails.vue
+++ src/views/GameDetails.vue
@@ -2,16 +2,45 @@
 <!-- The edit button can replace the pop-up with the v-form -->
 <template>
   <div>
-    <p>{{ game.title }} ({{ game.year }})</p>
-    <p>{{ game.minplayers }}-{{ game.maxplayers }}</p>
+   <!-- {{ this.$route.params.code}} -->
+    <p>Title:{{ title }}, Year: ({{ year }})</p>
+    <p>Number of players:{{ minplayers }}-{{ maxplayers }}</p>
+    <p>Description: {{description}}</p>
   </div>
 </template>
 
 <script>
-  export default {
-    setup() { 
-      // const route = this.route
-      // TODO: setup const game using caching + axios
+import axios from 'axios';
+
+export default {
+  name: "Game Detail",
+  mounted: function() {
+    this.populateFields()
+  },
+  data: () => ({
+    title: null,
+    year: null,
+    minplayers: null,
+    maxplayers: null
+  }),
+  methods: {
+    async fetchData() {
+      try {
+        var url = 'http://127.0.0.1:8000/api/v1/game/' + this.$route.params.code + '/'
+        return await axios.get(url);
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async populateFields() {
+      let res = (await this.fetchData()).data
+      let elem = res["data"]
+      this.minplayers = Math.min.apply(Math, elem.players)
+      this.maxplayers = Math.max.apply(Math, elem.players)
+      this.title = elem.title
+      this.year = elem.year
+      this.description = elem.description
     }
   }
+}
 </script>
