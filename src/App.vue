3356c13eae12ea176e7f4c42113f47fd5b8ca3cb
Lysire <slackertux13.37@gmail.com>
2022-01-07 15:31:15 +0800

    App skeleton and cards

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <!-- top bar -->
+    <v-app-bar app>
+      <v-toolbar-title>Games</v-toolbar-title>
+    </v-app-bar>
+
+    <!-- main container -->
+    <v-main class="grey lighten-2">
+      <v-container>
+
+        <!-- advanced search component -->
+        <v-expansion-panels accordion>
+          <v-expansion-panel>
+            <v-expansion-panel-header>Advanced Search</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-text-field dense filled v-model="gameinfo.title" placeholder="Title"></v-text-field>
+
+              <v-row no-gutters>
+                <v-col cols="3">
+                  <v-text-field dense filled v-model="gameinfo.genre" placeholder="Genre"></v-text-field>
+                </v-col>
+                <v-spacer vertical class="mx-4"></v-spacer>
+                <v-col cols="3">
+                  <v-text-field dense filled v-model="gameinfo.year" placeholder="Year"></v-text-field>
+                </v-col>
+                <v-spacer vertical class="mx-4"></v-spacer>
+                <v-col cols="1" class="pt-3">
+                  Players:
+                </v-col>
+                <v-col cols="1">
+                  <v-select
+                    v-model="gameinfo.minnumber"
+                    :items="players"
+                    dense
+                    chips
+                    outlined
+                  ></v-select>
+                </v-col>
+                <v-col cols="0" class="pt-3 pl-10" align-content="center">
+                  to
+                </v-col>
+                <v-col cols="1">
+                  <v-select
+                      v-model="gameinfo.maxnumber"
+                      :items="players"
+                      dense
+                      chips
+                      outlined
+                    ></v-select>
+                </v-col>
+              </v-row>
+
+              <v-row no-gutters>
+                <v-col align="end">
+                  <v-btn color="primary">Submit</v-btn>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+
+        <!-- store cards later -->
+        <v-row class="mt-4">
+          <template v-for="n in 4">
+            <v-col
+              v-for="j in 6"
+              :key="`${n}${j}`"
+              cols="6"
+              md="2"
+            >
+              <GameCard></GameCard>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameCard from './components/GameCard'
 
 export default {
   name: 'App',
+  data: () => ({
+    gameinfo: {
+      title: '',
+      genre: null,
+      year: null, 
+      minplayers: null,
+      maxplayers: null
+    },
+    genres: [],
+    players: [1,2,3,4,5,6,7,8,9,10]
+  }),
   components: {
-    HelloWorld
+    GameCard
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/GameCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container>
+        <v-card class="mx-auto" max-width="300" @click=onclick>
+            <v-card-title>
+                {{ title }}
+                <div class="text--secondary ml-1">({{ year }})</div>
+            </v-card-title>
+
+            <v-card-subtitle>
+                {{ minplayers }}-{{ maxplayers }} players
+            </v-card-subtitle>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: { default: "", type: String },
+            year: { default: 0, type: Number },
+            minplayers: { default: 0, type: Number},
+            maxplayers: { default: 0, type: Number}
+        },
+        methods: {
+            onclick: function() {}
+        }
+    }
+</script>
